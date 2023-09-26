11edd5e2380d6d72706dde04dabf2f99ba4b1cb8
JoaoIto <joaovictorpfr@gmail.com>
2023-09-26 15:34:19 -0300

    add mongoose and save messages

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,69 @@
-export default function ChatPage(){
-    return <>
-    <h1>Chat Page: </h1>
-    </>
+"use client"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function ChatPage() {
+    const [mensagem, setMensagem] = useState('');
+    const [listaDeMensagens, setListaDeMensagens] = useState<Array<{ de: string; texto: string }>>([]);
+    const [de, setDe] = useState('JoaoIto');
+
+    useEffect(() => {
+        // Função para buscar as mensagens da API
+        const getMensagens = async () => {
+            try {
+                const response = await axios.get('/api/chat/mensagens');
+                setListaDeMensagens(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar mensagens:', error);
+            }
+        };
+
+        getMensagens();
+    }, []);
+
+    const handleNovaMensagem = async () => {
+        if (!mensagem) {
+            return;
+        }
+
+        try {
+            // Enviar a nova mensagem para a API
+            await axios.post('/api/chat/mensagens', { de, texto: mensagem });
+
+            // Atualizar a lista de mensagens
+            setListaDeMensagens([
+                ...listaDeMensagens,
+                { de, texto: mensagem },
+            ]);
+
+            // Limpar a caixa de mensagem
+            setMensagem('');
+        } catch (error) {
+            console.error('Erro ao enviar mensagem:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Chat</h1>
+            <div>
+                <ul>
+                    {listaDeMensagens.map((mensagem, index) => (
+                        <li key={index}>
+                            {mensagem.de}: {mensagem.texto}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <input
+                    type="text"
+                    value={mensagem}
+                    onChange={(e) => setMensagem(e.target.value)}
+                    placeholder="Digite sua mensagem..."
+                />
+                <button onClick={handleNovaMensagem}>Enviar</button>
+            </div>
+        </div>
+    );
 }
