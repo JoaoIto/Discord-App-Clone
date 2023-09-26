740373a4c9a43a3a27f892a92225f5bb839f28e5
JoaoIto <joaovictorpfr@gmail.com>
2023-09-26 06:56:39 -0300

    add icon discord to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Discord Clone',
-  description: 'A clone UI/UX for Discord',
+    title: 'Discord Clone',
+    description: 'A clone UI/UX for Discord',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <link rel="shortcut icon" href="./assets/discord.png" type="image/x-icon"/>
+        </head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
