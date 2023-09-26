3cd4c6ca2fc511a896a1d8e01c012e2486c1a7ea
JoaoIto <joaovictorpfr@gmail.com>
2023-09-26 07:09:30 -0300

    button entrar

--- app/components/SignIn/SignInPage.tsx
+++ app/components/SignIn/SignInPage.tsx
@@ -24,19 +24,22 @@ export default function PaginaInicial() {
             <Head>
                 <title>Discord App</title>
             </Head>
-            <div className="flex items-center justify-center min-h-[600] bg-primary bg-cover bg-blend-multiply bg-fixed">
-                <div className="flex items-center justify-between flex-col h-full sm:flex-row w-full max-w-screen-lg rounded p-8 m-4 shadow-md bg-green-700">
+            <div className="flex items-center justify-center min-h-[800] bg-primary bg-cover bg-blend-multiply bg-fixed">
+                <div className="flex items-center justify-between flex-col border border-solid border-2 border-slate-300 h-full sm:flex-row w-full max-w-screen-lg rounded p-8 m-4 shadow-md bg-green-700">
                     <form onSubmit={handleFormSubmit} className="flex flex-col items-center justify-center w-full sm:w-1/2 mb-4 sm:mb-0">
                         <h2 className="text-2xl font-semibold text-white mb-4">Boas vindas de volta!</h2>
+
+                        <label className={`text-xl`} htmlFor="username">Insira seu nome de usuário: </label>
                         <input
+                            id="username"
                             value={username}
                             onChange={handleUsernameChange}
-                            className="w-60 h-14 p-4 rounded text-white font-medium bg-gray-900 border-primary-500"
+                            className="w-80 h-14 p-4 rounded border border-solid border-2 border-slate-300 text-white font-medium bg-gray-900 border-primary-500"
                         />
-                        <button
+                        <input
                             value="Entrar"
                             type="submit"
-                            className="text-white bg-primary-500"
+                            className="w-80 h-14 flex items-center cursor-pointer my-4 justify-center p-4 font-medium text-2xl text-center rounded border border-solid border-2 border-slate-300 text-white bg-blue-500"
                         />
                     </form>
                     <div className="flex border border-solid border-2 border-slate-300 flex-col items-center w-full sm:w-1/2 max-w-xs p-4 bg-gray-800 border border-gray-900 rounded flex-1 min-h-40">
