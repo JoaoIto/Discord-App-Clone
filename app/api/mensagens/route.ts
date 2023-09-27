593aa97e7792c510f5d6dd3187bc48a03dc87ad7
JoaoIto <joaovictorpfr@gmail.com>
2023-09-27 16:24:33 -0300

    add NextResponse

--- app/api/mensagens/route.ts
+++ app/api/mensagens/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import connectToDatabase from '../../../app/database/database';
-import { NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 
 // Função de teste para conexão do banco de dados:
 // export async function GET(req: NextApiRequest, res: NextApiResponse) {
@@ -17,7 +17,7 @@ import { NextResponse } from 'next/server';
 //     }
 // }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
             // Teste a conexão com o banco de dados e retorne o resultado
