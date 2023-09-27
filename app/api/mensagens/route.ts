574cca8e21ab3f9565eb33b4ab36d8c86032f902
JoaoIto <joaovictorpfr@gmail.com>
2023-09-27 17:26:14 -0300

    unescessary lines in route get

--- app/api/mensagens/route.ts
+++ app/api/mensagens/route.ts
@@ -2,21 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import connectToDatabase from '../../../app/database/database';
 import {NextRequest, NextResponse} from 'next/server';
 
-// Função de teste para conexão do banco de dados:
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//     if (req.method === 'GET') {
-//         try {
-//             // Teste a conexão com o banco de dados e retorne o resultado
-//             const result = await connectToDatabase();
-//             return NextResponse.json({ message: result });
-//         } catch (error: any) {
-//             return NextResponse.json({ error: error.message });
-//         }
-//     } else {
-//         return NextResponse.error();
-//     }
-// }
-
 export async function GET(req: NextRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
