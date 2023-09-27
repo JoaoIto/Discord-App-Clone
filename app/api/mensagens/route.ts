44779bda8f0eb1c9cf10d6ebea70be2a491d1911
JoaoIto <joaovictorpfr@gmail.com>
2023-09-27 15:55:08 -0300

    conectado ao banco de dados

--- app/api/mensagens/route.ts
+++ app/api/mensagens/route.ts
@@ -3,12 +3,28 @@ import connectToDatabase from '../../../app/database/database';
 import { NextResponse } from 'next/server';
 
 // Função de teste para conexão do banco de dados:
+// export async function GET(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'GET') {
+//         try {
+//             // Teste a conexão com o banco de dados e retorne o resultado
+//             const result = await connectToDatabase();
+//             return NextResponse.json({ message: result });
+//         } catch (error: any) {
+//             return NextResponse.json({ error: error.message });
+//         }
+//     } else {
+//         return NextResponse.error();
+//     }
+// }
+
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         try {
             // Teste a conexão com o banco de dados e retorne o resultado
-            const result = await connectToDatabase();
-            return NextResponse.json({ message: result });
+            const db = await connectToDatabase();
+            const collection = db.collection('mensagens')
+            const result = await collection.find({}).toArray();
+            return NextResponse.json({ mensagens: result });
         } catch (error: any) {
             return NextResponse.json({ error: error.message });
         }
--- app/database/database.ts
+++ app/database/database.ts
@@ -1,44 +1,6 @@
-// // mongodb.js
-//
-// // import { MongoClient } from 'mongodb'
-// //
-// // const uri = process.env.DATABASE_URL as string
-// //
-// // let client = new MongoClient(uri);
-// // let clientPromise = client.connect()
-// // export default clientPromise
-// //
-//
-// import mongoose from 'mongoose';
-//
-// const connectToDatabase = async () => {
-//     try {
-//         await mongoose.connect(process.env.DATABASE_URL as string);
-//         console.log('Conectado ao MongoDB');
-//     } catch (error) {
-//         console.error('Erro na conexão com o MongoDB:', error);
-//     }
-// };
-//
-// export default connectToDatabase;
-//
-//
-// // import mongoose from 'mongoose';
-// //
-// // const connectToDatabase = async () => {
-// //     try {
-// //         await mongoose.connect(process.env.DATABASE_URL as string);
-// //         console.log('Conectado ao MongoDB');
-// //     } catch (error) {
-// //         console.error('Erro na conexão com o MongoDB:', error);
-// //     }
-// // };
-// //
-// // export default connectToDatabase;
-
-
 import { MongoClient } from 'mongodb';
 
+// Configuração da conexão com o banco de dados
 const client = new MongoClient(process.env.DATABASE_URL as string);
 
 async function connectToDatabase() {
@@ -46,16 +8,10 @@ async function connectToDatabase() {
         await client.connect();
         const db = client.db();
         console.log('Conectado ao MongoDB');
-
-        // Faça um teste simples, por exemplo, contar o número de documentos em uma coleção
-        const collection = db.collection('mensagens'); // Substitua 'suaColecao' pelo nome da sua coleção
-        const count = await collection.countDocuments({});
-        console.log(`Número de documentos na coleção: ${count}`);
-
-        return 'Conexão bem-sucedida!';
+        return db; // Retorne a instância do banco de dados
     } catch (error) {
         console.error('Erro na conexão com o MongoDB:', error);
-        return 'Erro na conexão com o MongoDB.';
+        throw new Error('Erro na conexão com o MongoDB.');
     }
 }
 
